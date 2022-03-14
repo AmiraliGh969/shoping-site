2ec2266ca801c11b3827f6b662264c5d5bd30c14
AmiraliGh969 <amirali.gh.program@gmail.com>
2022-03-14 15:50:34 +0330

    add other validate

--- src/pages/Account/signup.vue
+++ src/pages/Account/signup.vue
@@ -7,28 +7,48 @@
             <form action="#" class="d-flex flex-column">
               <div class="d-flex flex-row">
                 <input type="text" v-model="$v.form.name.firstname.$model" class="form-control mt-3 me-1"
-                       :class="{'error-input':!$v.form.name.firstname.required && $v.form.name.firstname.$dirty}"
-                       placeholder="First name">
+                  :class="{'error-input':!$v.form.name.firstname.required && $v.form.name.firstname.$dirty}"
+                  placeholder="First name">
                 <input type="text" v-model="$v.form.name.lastname.$model"
-                       :class="{'error-input':!$v.form.name.lastname.required && $v.form.name.lastname.$dirty}"
-                       class="form-control mt-3 ms-1" placeholder="Last name">
+                  :class="{'error-input':!$v.form.name.lastname.required && $v.form.name.lastname.$dirty}"
+                  class="form-control mt-3 ms-1" placeholder="Last name">
               </div>
-              <input type="text" v-model="form.email" class="form-control mt-3" placeholder="Email address">
-              <input type="text" v-model="form.username" class="form-control mt-3" placeholder="Username">
-              <input type="password" v-model="form.password" class="form-control mt-3" placeholder="Password">
-
+              <input type="email" v-model="$v.form.email.$model" 
+                class="form-control mt-3" 
+                :class="{'error-input':!$v.form.email.required && $v.form.email.$dirty}" 
+                placeholder="Email address">
+              <input type="text" v-model="$v.form.username.$model" 
+                class="form-control mt-3" placeholder="Username"
+                :class="{'error-input':!$v.form.username.required && $v.form.username.$dirty}" 
+                >
+              <input type="password" v-model="$v.form.password.$model" 
+                class="form-control mt-3" placeholder="Password"
+                :class="{'error-input':!$v.form.password.required && $v.form.password.$dirty}" 
+                >
               <input type="password" class="form-control mt-3" placeholder="Repeat the password">
-              <input id="city" type="text" v-model="form.address.city" class="form-control mt-3" placeholder="city">
-              <input id="street" type="text" v-model="form.address.street" class="form-control mt-3"
-                     placeholder="street">
-              <input id="number" type="text" v-model="form.address.number" class="form-control mt-3"
-                     placeholder="number">
-              <input id="zipcode" type="text" v-model="form.address.zipcode" class="form-control mt-3"
-                     placeholder="zipcode">
-              <input id="geolocationlat" type="text" v-model="form.address.geolocation.lat" class="form-control mt-3"
-                     placeholder="lat">
-              <input id="geolocationlong" type="text" v-model="form.address.geolocation.long" class="form-control mt-3"
-                     placeholder="long">
+              <input id="city" type="text" v-model="$v.form.address.city.$model" 
+                :class="{'error-input':!$v.form.address.city.required && $v.form.address.city.$dirty}" 
+                class="form-control mt-3" placeholder="city">
+              <input id="street" type="text" v-model="$v.form.address.street.$model" 
+                class="form-control mt-3"
+                :class="{'error-input':!$v.form.address.street.required && $v.form.address.street.$dirty}" 
+                placeholder="street">
+              <input id="number" type="text" v-model="$v.form.address.number.$model" 
+                class="form-control mt-3"
+               :class="{'error-input':!$v.form.address.number.required && $v.form.address.number.$dirty}" 
+                placeholder="number">
+              <input id="zipcode" type="text" v-model="$v.form.address.zipcode.$model" 
+                class="form-control mt-3"
+               :class="{'error-input':!$v.form.address.zipcode.required && $v.form.address.zipcode.$dirty}" 
+                placeholder="zipcode">
+              <input id="geolocationlat" type="text" v-model="$v.form.address.geolocation.lat.$model" 
+                class="form-control mt-3"
+               :class="{'error-input':!$v.form.address.geolocation.lat.required && $v.form.address.geolocation.lat.$dirty}" 
+                placeholder="lat">
+              <input id="geolocationlong" type="text" v-model="$v.form.address.geolocation.long.$model" 
+                class="form-control mt-3"
+               :class="{'error-input':!$v.form.address.geolocation.long.required && $v.form.address.geolocation.long.$dirty}" 
+                placeholder="long">
               <br>
               <button @click.prevent="registerUser" type="submit" class="btn btn-success my-2">Sing up</button>
             </form>
@@ -58,13 +78,23 @@ export default {
   },
   validations: {
     form: {
+      email: {required, email},
+      username: {required},
+      password: {required, minLength: minLength(6)},
       name: {
         firstname: {required},
         lastname: {required}
       },
-      email: {required, email},
-      username: {required},
-      password: {required, minLength: minLength(6)},
+      address:{
+        city:{required},
+        street:{required},
+        number:{required},
+        zipcode:{required},
+        geolocation:{
+          lat:{required},
+          long:{required}
+        }
+      },
     }
   },
 };
